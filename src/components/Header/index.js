a28a23a53650fa1c472fb3ac1caefb0fdf56b58f
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-03-19 17:20:39 -0400

    display header and navbar

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../Nav";
+
+function Header () {
+    return (
+        <header>
+            <h1>Christiana C. S. Morales</h1>
+            <Nav></Nav>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li><span>About Me</span></li>
+                <li><span>My Projects</span></li>
+                <li><span>Contact Me</span></li>
+                <li><span>Resume</span></li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Nav;
