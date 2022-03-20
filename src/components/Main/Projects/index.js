35f964c70c2a2cd87508b0aa0f1bfeb58a108f24
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-03-20 17:44:51 -0400

    add project info and photos; everything displays correctly

--- src/components/Main/Projects/index.js
+++ src/components/Main/Projects/index.js
@@ -3,55 +3,51 @@ import React, { useState } from "react";
 function Projects() {
     const [featuredProjects] = useState([
         {
-            title: "",
-            techsUsed: "",
-            deployedUrl: "",
-            ghRepoUrl: "",
-            imagePath: ""
+            title: "The Sheet Show",
+            deployedUrl: "https://sheet-show.herokuapp.com/",
+            ghRepoUrl: "https://github.com/NicaVulcan/The-Sheet-Show",
+            imagePath: "sheet-show"
         },
         {
-            title: "",
-            techsUsed: "",
-            deployedUrl: "",
-            ghRepoUrl: "",
-            imagePath: ""
+            title: "Budget Tracker - PWA",
+            deployedUrl: "https://safe-crag-64410.herokuapp.com/",
+            ghRepoUrl: "https://github.com/NicaVulcan/budget-tracker",
+            imagePath: "budget-tracker"
         },
         {
-            title: "",
-            techsUsed: "",
-            deployedUrl: "",
-            ghRepoUrl: "",
-            imagePath: ""
+            title: "Tech Blog",
+            deployedUrl: "https://safe-dusk-00550.herokuapp.com/",
+            ghRepoUrl: "https://github.com/NicaVulcan/tech-blog",
+            imagePath: "tech-blog"
         },
         {
-            title: "",
-            techsUsed: "",
-            deployedUrl: "",
-            ghRepoUrl: "",
-            imagePath: ""
+            title: "Poke-Dinner",
+            deployedUrl: "https://vinssm.github.io/PokeDinner/index.html",
+            ghRepoUrl: "https://github.com/NicaVulcan/PokeDinner",
+            imagePath: "poke-dinner"
         },
         {
-            title: "",
-            techsUsed: "",
-            deployedUrl: "",
-            ghRepoUrl: "",
-            imagePath: ""
+            title: "Note Taker",
+            deployedUrl: "https://frozen-crag-25746.herokuapp.com/",
+            ghRepoUrl: "https://github.com/NicaVulcan/note-taker",
+            imagePath: "note-taker"
         },
         {
-            title: "",
-            techsUsed: "",
-            deployedUrl: "",
-            ghRepoUrl: "",
-            imagePath: ""
-        }
+            title: "Weather Dashboard",
+            deployedUrl: "https://nicavulcan.github.io/weather-dashboard/",
+            ghRepoUrl: "https://github.com/NicaVulcan/weather-dashboard",
+            imagePath: "weather-dashboard"
+        },
     ])
     return (
         <div>
             <h2>My Projects</h2>
             <div>
                 {featuredProjects.map((project, i) => (
-                    <div className="project-card">
-                        <img src={project.imagePath} alt={project.title}></img>
+                    <div className="project-card" key={project.title}>
+                        <div>
+                            <img src={`/assets/images/project-screenshots/${project.imagePath}.png`} alt={project.title}></img>
+                        </div>
                         <h4>{project.title}</h4>
                         <p>{project.techsUsed}</p>
                         <p>
