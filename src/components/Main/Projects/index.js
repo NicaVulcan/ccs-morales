efd555f637785a5d6917022a1feb445a7c733b5b
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2023-09-06 15:59:01 -0400

    updated avatar and resume

--- src/components/Main/Projects/index.js
+++ src/components/Main/Projects/index.js
@@ -5,23 +5,23 @@ function Projects() {
     const [featuredProjects] = useState([
         {
             title: "Calendari",
-            deployedUrl: "https://calendari-day.herokuapp.com",
+            // deployedUrl: "https://calendari-day.herokuapp.com",
             ghRepoUrl: "https://github.com/ErikPlachta/Calendari",
             imagePath: "calendari",
             description: "Calendari is a full-stack, concept scheduling application that allows small businesses to create an account, and provides them with a unique url that clients can use to schedule appointments based on the business' availability.",
             techs: ["React", "CSS", "Node", "Express", "MongoDB", "Mongoose", "GraphQL", "Apollo", "Apollo Playground", "MongoDB Atlas", "Heroku"]
         },
-        // {
-        //     title: "The Sheet Show",
-        //     deployedUrl: "https://sheet-show.herokuapp.com/",
-        //     ghRepoUrl: "https://github.com/NicaVulcan/The-Sheet-Show",
-        //     imagePath: "sheet-show",
-        //     description: "The Sheet Show is a full stack application that allows a user to automatically generate a Dungeons and Dragons character sheet based on user preferences of 'Class' and 'Race'. This application randomizes character stats based on the chosen Class and Race parameters.",
-        //     techs: ["Handlebars", "CSS", "Bootstrap", "Character Sheet CSS", "JavaScript", "DnD5e API", "Node", "Express", "Express Session", "Dotenv", "Bcrypt", "MySQL", "Sequelize", "Insomnia", "JawsDB", "Heroku"]
-        // },
+        {
+            title: "The Sheet Show",
+            // deployedUrl: "https://sheet-show.herokuapp.com/",
+            ghRepoUrl: "https://github.com/NicaVulcan/The-Sheet-Show",
+            imagePath: "sheet-show",
+            description: "The Sheet Show is a full stack application that allows a user to automatically generate a Dungeons and Dragons character sheet based on user preferences of 'Class' and 'Race'. This application randomizes character stats based on the chosen Class and Race parameters.",
+            techs: ["Handlebars", "CSS", "Bootstrap", "Character Sheet CSS", "JavaScript", "DnD5e API", "Node", "Express", "Express Session", "Dotenv", "Bcrypt", "MySQL", "Sequelize", "Insomnia", "JawsDB", "Heroku"]
+        },
         {
             title: "Budget Tracker",
-            deployedUrl: "https://safe-crag-64410.herokuapp.com/",
+            // deployedUrl: "https://safe-crag-64410.herokuapp.com/",
             ghRepoUrl: "https://github.com/NicaVulcan/budget-tracker",
             imagePath: "budget-tracker",
             description: "Budget tracker is a full-stack, progressive web application meant to help the user keep track of daily transactions. This application is downloadable, and can operate offline by saving offline transactions to the browser's cache using IndexedDB, and posting these transactions once connectivity is restored.",
@@ -29,7 +29,7 @@ function Projects() {
         },
         {
             title: "Tech Blog",
-            deployedUrl: "https://safe-dusk-00550.herokuapp.com/",
+            // deployedUrl: "https://safe-dusk-00550.herokuapp.com/",
             ghRepoUrl: "https://github.com/NicaVulcan/tech-blog",
             imagePath: "tech-blog",
             description: "Tech Blog is a concept blog application that allows users to create an account, or login if an account has previously been created. When logged in, a user can visit their personal dashboard, create new posts, update or delete their posts, and comment on other user's posts.",
@@ -58,19 +58,19 @@ function Projects() {
         },
         {
             title: "Note Taker",
-            deployedUrl: "https://frozen-crag-25746.herokuapp.com/",
+            // deployedUrl: "https://frozen-crag-25746.herokuapp.com/",
             ghRepoUrl: "https://github.com/NicaVulcan/note-taker",
             imagePath: "note-taker",
             description: "Note Taker is a full-stack application that uses a local database. This application allows the user to create, read, update, or delete notes.",
             techs: ["HTML", "CSS", "Bootstrap", "FontAwesome", "JavaScript", "Node", "Express", "JSON"]
         },
-        // {
-        //     title: "Team Profile Generator",
-        //     ghRepoUrl: "https://github.com/NicaVulcan/team-profile-generator",
-        //     imagePath: "team-profile-generator",
-        //     description: "Team Profile Generator is a CLI application that prompts the user for information on team members to generate an HTML file that displays a team profile.",
-        //     techs: ["HTML", "CSS", "Node", "Inquirer", "Jest"]
-        // },
+        {
+            title: "Team Profile Generator",
+            ghRepoUrl: "https://github.com/NicaVulcan/team-profile-generator",
+            imagePath: "team-profile-generator",
+            description: "Team Profile Generator is a CLI application that prompts the user for information on team members to generate an HTML file that displays a team profile.",
+            techs: ["HTML", "CSS", "Node", "Inquirer", "Jest"]
+        },
     ])
 
     return (
